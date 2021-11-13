dc0504cee57737fb200b0a3012106df4eb19eeb8
Wanderson Barradas <wandersonbarradas07@gmail.com>
2021-11-13 18:55:35 -0300

    adicionando o modal

--- assets/scripts/restaurantes.js
+++ assets/scripts/restaurantes.js
@@ -1,4 +1,41 @@
 let restaurante = [
-    {nome: 'burgão lanches', endereco: { rua: 'Av. Padre Joaquim Nonato', numero: '540', bairro: 'centro', cidade: 'demerval lobão - pi'}, valormindely: 10, status: {status: 'fechado', desc: 'Fechado agora'}, img: 'https://deliverydl.com.br/uploads/images/2021/10/img-20211013-wa0013.jpg'},
-    {nome: 'Açaí Mel', endereco: { rua: 'rua', numero: '757', bairro: 'Cidade Nova', cidade: 'demerval lobão - pi'}, valormindely: 10, status: {status: 'aberto', desc: 'aberto agora'}, img: 'https://deliverydl.com.br/uploads/images/2021/10/8896236c-dea9-4f59-add1-d8cd2771f633.jpeg'}
-]
+  {
+    nome: "burgão lanches",
+    endereco: {
+      rua: "Av. Padre Joaquim Nonato",
+      numero: "540",
+      bairro: "centro",
+      cidade: "demerval lobão - pi",
+    },
+    valormindely: 10,
+    status: { status: "fechado", desc: "Fechado agora" },
+    img: "https://deliverydl.com.br/uploads/images/2021/10/img-20211013-wa0013.jpg",
+    capa: "https://deliverydl.com.br/uploads/images/2021/10/images.jpeg",
+  },
+  {
+    nome: "Açaí Mel",
+    endereco: {
+      rua: "rua",
+      numero: "757",
+      bairro: "Cidade Nova",
+      cidade: "demerval lobão - pi",
+    },
+    valormindely: 10,
+    status: { status: "aberto", desc: "aberto agora" },
+    img: "https://deliverydl.com.br/uploads/images/2021/10/8896236c-dea9-4f59-add1-d8cd2771f633.jpeg",
+    capa: "	https://deliverydl.com.br/uploads/images/2021/10/images-21.jpeg",
+  },
+  {
+    nome: "Doce Capricho",
+    endereco: {
+      rua: "Av. Francisco Luis de Morais",
+      numero: "1305",
+      bairro: "Centro",
+      cidade: "demerval lobão - pi",
+    },
+    valormindely: 15,
+    status: { status: "fechado", desc: "fechado agora" },
+    img: "https://deliverydl.com.br/uploads/images/2021/10/img-20210527-wa0097.jpg",
+    capa: "	https://deliverydl.com.br/uploads/images/2021/10/images-68.jpeg",
+  },
+];
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,41 +1,76 @@
-let c = (el)=>document.querySelector(el);
-let cs = (el)=>document.querySelectorAll(el);
+let c = (el) => document.querySelector(el);
+let cs = (el) => document.querySelectorAll(el);
 
-restaurante.map((item, index)=>{
-    let cadastro = c('.models .container-res').cloneNode(true);
+let modalkey = 0;
 
+restaurante.map((item, index) => {
+  let cadastro = c(".models .container-res").cloneNode(true);
 
-    cadastro.querySelector('.box-img img').src = item.img
-    cadastro.querySelector('.name-res').innerHTML = item.nome;
-    cadastro.querySelector('.city').innerHTML = item.endereco.cidade;
-    cadastro.querySelector('.endereco-res').innerHTML = `${item.endereco.rua}, ${item.endereco.numero}, ${item.endereco.bairro}.`;
-    cadastro.querySelector('.valordely-res span').innerHTML = `R$ ${item.valormindely.toFixed(2)}`
-    cadastro.querySelector('.status-res').innerHTML = item.status.desc;
-    cadastro.querySelector('.status-res').classList.add(item.status.status)
+  cadastro.setAttribute("data-key", index);
+  cadastro.querySelector(".box-img img").src = item.img;
+  cadastro.querySelector(".name-res").innerHTML = item.nome;
+  cadastro.querySelector(".city").innerHTML = item.endereco.cidade;
+  cadastro.querySelector(
+    ".endereco-res"
+  ).innerHTML = `${item.endereco.rua}, ${item.endereco.numero}, ${item.endereco.bairro}.`;
+  cadastro.querySelector(
+    ".valordely-res span"
+  ).innerHTML = `R$ ${item.valormindely.toFixed(2)}`;
+  cadastro.querySelector(".status-res").innerHTML = item.status.desc;
+  cadastro.querySelector(".status-res").classList.add(item.status.status);
 
+  //Abrindo modal
+  cadastro.querySelector("a").addEventListener("click", (e) => {
+    e.preventDefault();
 
+    let key = cadastro.getAttribute("data-key");
+    c(".capa-modal").style.background = `url(${restaurante[key].capa})`;
+    c(".capa-modal").style.backgroundPosition = "center";
+    c(".capa-modal").style.backgroundSize = "cover";
+    c(".logo-res-modal").src = restaurante[key].img;
+    c(".nome-res-modal").innerHTML = restaurante[key].nome;
+    c(".content-main-modal .status-res").innerHTML =
+      restaurante[key].status.desc;
+    c(".content-main-modal .status-res").classList.add(
+      restaurante[key].status.status
+    );
 
+    abrirModalRes();
+  });
 
-    c('.container-cadastros').append(cadastro);
-})
-
+  c(".container-cadastros").append(cadastro);
+});
 
 function decidirmenu() {
-    if(window.scrollY === 0){
-        c('.header-line').classList.remove('menu-fixed');
-    }else {
-        c('.header-line').classList.add('menu-fixed');
-    }
+  if (window.scrollY === 0) {
+    c(".header-line").classList.remove("menu-fixed");
+    c(".header-line-modal").classList.remove("menu-fixed");
+  } else {
+    c(".header-line").classList.add("menu-fixed");
+    c(".header-line-modal").classList.add("menu-fixed");
+  }
 }
 
-window.addEventListener('scroll', decidirmenu);
-c('.menu-header ul').style.right = '-100vw';
+window.addEventListener("scroll", decidirmenu);
+c(".menu-header ul").style.right = "-100vw";
 
 function menuShow() {
+  if (c(".menu-header ul").style.right === "-100vw") {
+    c(".menu-header ul").style.right = "0";
+  } else {
+    c(".menu-header ul").style.right = "-100vw";
+  }
+}
 
-    if(c('.menu-header ul').style.right === '-100vw') {
-        c('.menu-header ul').style.right = '0';
-    }else {
-        c('.menu-header ul').style.right = '-100vw';
-    }
+function abrirModalRes() {
+  c(".section-header").style.opacity = "0";
+  c(".restaurantes").style.opacity = "0";
+
+  c(".modal-res").style.opacity = "0";
+  c(".modal-res").style.display = "block";
+  setInterval(() => {
+    c(".modal-res").style.opacity = "1";
+    c(".section-header").style.display = "none";
+    c(".restaurantes").style.display = "none";
+  }, 500);
 }
